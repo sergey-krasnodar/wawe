c6433fc65024798d86d96b6a18e2b1e77cf21962
sergey-krasnodar <serega.ru71@mail.ru>
2022-10-21 20:45:31 +0300

    pravka-1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 $(function(){
+
+  $(".menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
   
   $('.blog__slider').slick({
     arrows: false,
     dots: true
   });
 
-  $('.menu__btn').on('click', function(){
+  $('.menu__btn, .menu a').on('click', function(){
     $('.menu__list').toggleClass('menu__list--active');
   });
   
